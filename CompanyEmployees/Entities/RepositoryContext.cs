494b9f51e2e468948e4f85650e937683bc41ce25
Emmanuel Nzekwe <emmanuelnzekwe24@gmail.com>
2021-01-14 02:26:52 +0100

    feat: add DbContext and database connection

---
+++ CompanyEmployees/Entities/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using CompanyEmployees.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Entities
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Company> Companies { set; get; }
+        public DbSet<Employee> Employies { set; get; }
+    }
+}
