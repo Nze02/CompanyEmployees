778c5737aaa52967fe6b7aa84c95b5b8005812da
Emmanuel Nzekwe <emmanuelnzekwe24@gmail.com>
2021-01-29 03:21:08 +0100

    feat: implement ArrayModelBinders in Companies controller

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -2,6 +2,7 @@
 using CompanyEmployees.Contracts;
 using CompanyEmployees.DataTransferObjects;
 using CompanyEmployees.Entities.Models;
+using CompanyEmployees.ModelBinders;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -73,7 +74,7 @@ namespace CompanyEmployees.Controllers
         }
 
         [HttpGet("collection/({ids})", Name = "CompanyCollection")]
-        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
         {
             if(ids == null)
             {
