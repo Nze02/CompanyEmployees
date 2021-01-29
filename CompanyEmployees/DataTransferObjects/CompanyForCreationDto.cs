7edae3250f138559f6fd44b13542dcd3a07b9034
Emmanuel Nzekwe <emmanuelnzekwe24@gmail.com>
2021-01-29 01:48:58 +0100

    feat: implement create both parent and child resources in Companies controller

--- CompanyEmployees/DataTransferObjects/CompanyForCreationDto.cs
+++ CompanyEmployees/DataTransferObjects/CompanyForCreationDto.cs
@@ -12,5 +12,7 @@ namespace CompanyEmployees.DataTransferObjects
         public string Address { get; set; }
 
         public string Country { get; set; }
+
+        public IEnumerable<EmployeeForCreationDto> Employees { get; set; }
     }
 }
